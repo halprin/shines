f8cd4708d6b898ff32e93cf2b4beb7dee32932d7
halprin <halprin@users.noreply.github.com>
2022-01-21 20:45:06 -0700

    Paint the UI

--- app/tick.rb
+++ app/tick.rb
@@ -11,6 +11,7 @@ def tick(args)
   collision_checking(args)
 
   args.outputs.sprites << args.state.sprites.all
+  display_ui(args)
 end
 
 def collision_checking(args)
@@ -28,21 +29,21 @@ def collision_for_blocks(args)
     touching_left_side_of_blocks = intersecting_blocks.select { |block| block.collision_side(hero) == 'LEFT' }
 
     if above_blocks.size() > 0
-      args.labels << [580, 400, 'Above Intersection']
+      args.outputs.debug << [args.grid.left, args.grid.top - 60, 'Above', -2, 0, 255, 255, 255].label
       hero.stand(above_blocks)
     else
       hero.unstand()
     end
 
     if touching_right_side_of_blocks.size() > 0
-      args.labels << [580, 500, 'Right Intersection']
+      args.outputs.debug << [args.grid.left + 100, args.grid.top - 60, 'Right', -2, 0, 255, 255, 255].label
       hero.stop_left_movement(touching_right_side_of_blocks)
     else
       hero.allow_left_movement()
     end
 
     if touching_left_side_of_blocks.size() > 0
-      args.labels << [580, 600, 'Left Intersection']
+      args.outputs.debug << [args.grid.left + 200, args.grid.top - 60, 'Left', -2, 0, 255, 255, 255].label
       hero.stop_right_movement(touching_left_side_of_blocks)
     else
       hero.allow_right_movement()
@@ -71,7 +72,30 @@ def left_pressed_no_right(args)
   return args.inputs.left && !args.inputs.right
 end
 
+def display_ui(args)
+  # background
+  args.outputs.solids << [args.grid.left, args.grid.top - 80, args.grid.right, args.grid.top]
+
+  # score
+  args.outputs.labels << [args.grid.left, args.grid.top, 'Score', 0, 0, 255, 255, 255]
+  args.outputs.labels << [args.grid.left, args.grid.top - 16, '%07d' % args.state.score, 5, 0, 255, 255, 255]
+
+  # time
+  args.outputs.labels << [args.grid.left + 150, args.grid.top, 'Time', 0, 0, 255, 255, 255]
+  seconds = '%05.2f' % (0.elapsed_time / 60 % 60)
+  minutes = '%02d' % (0.elapsed_time / 60 / 60 % 60)
+  hours = '%02d' % (0.elapsed_time / 60 / 60 / 60)
+  args.outputs.labels << [args.grid.left + 150, args.grid.top - 16, "#{hours}:#{minutes}:#{seconds}", 5, 0, 255, 255, 255]
+
+  # lives
+  args.outputs.labels << [args.grid.left + 350, args.grid.top, 'Lives', 0, 0, 255, 255, 255]
+  args.outputs.labels << [args.grid.left + 350, args.grid.top - 16, args.state.lives, 5, 0, 255, 255, 255]
+end
+
 def initialize(args)
+  args.state.score = 0
+  args.state.lives = 5
+
   read_level(args)
 
   args.state.sprites.all = [args.state.sprites.hero]
