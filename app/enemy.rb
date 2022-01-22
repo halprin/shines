23b1db1ec275f4fe6a5ff5403aed883986f2f3d8
halprin <halprin@users.noreply.github.com>
2022-01-22 15:39:49 -0700

    Get looping enemy animations working

--- app/enemy.rb
+++ app/enemy.rb
@@ -11,10 +11,20 @@ class Enemy < Sprite
 
     @path_type = path_type
     @duration = duration
+
+    @start_tick = 0
+    @previous_progress = 0
   end
 
   def calculate(args)
-    progress = args.easing.ease(0, args.state.tick_count, @duration * 60, :identity)
+
+    if @previous_progress >= 1
+      # reset the start as being now since we fulfilled the entire animation of the enemy
+      @start_tick = args.state.tick_count
+    end
+
+    progress = args.easing.ease(@start_tick, args.state.tick_count, @duration * 60, :identity)
+    @previous_progress = progress
 
     total_delta_x = @ending_x - @starting_x
     total_delta_y = @ending_y - @starting_y
