57cdac324d239ea36c9d5f9c6a07e79f9521ac6f
halprin <halprin@users.noreply.github.com>
2022-01-23 13:28:27 -0700

    Rename the scene files

--- app/main.rb
+++ app/main.rb
@@ -4,13 +4,13 @@ require '/app/scene.rb'
 require '/app/sprite.rb'
 
 # main game scene
-require '/app/main_game/scene.rb'
+require '/app/main_game/main_game_scene.rb'
 require '/app/main_game/hero.rb'
 require '/app/main_game/enemy.rb'
 require '/app/main_game/block.rb'
 
 # game over scene
-require '/app/game_over/scene.rb'
+require '/app/game_over/game_over_scene.rb'
 
 # final tick
 require '/app/tick.rb'
